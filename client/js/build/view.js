875b93743c016226a793dce0154cdadb7a62dff1
Derrick J Chie <djchie.dev@gmail.com>
2015-09-22 17:14:26 -0700

    Modularizes the react views

--- client/js/build/view.js
+++ client/js/build/view.js
@@ -1,241 +1,239 @@
 
-/* ---------- Search Form View ---------- */
+// /* ---------- Search Form View ---------- */
 
-var SearchInput = React.createClass({displayName: "SearchInput",
-  searchInputHandler: function () {
-    var searchString = this.refs.searchInput.getDOMNode().value;
-    this.props.searchInputHandler(searchString);
-  },
-  render: function () {
-    return (
-      React.createElement("div", {className: "searchInput input-group col-top col-centered col-md-6"}, 
-        React.createElement("label", {for: "searchInput"}, "Search"), 
-        React.createElement("input", {id: "searchInput", className: "form-control", type: "text", ref: "searchInput", placeholder: "Name, Description, etc...", onChange: this.searchInputHandler})
-      )
-    );
-  }
-});
+// var SearchInput = React.createClass({
+//   searchInputHandler: function () {
+//     var searchString = this.refs.searchInput.getDOMNode().value;
+//     this.props.searchInputHandler(searchString);
+//   },
+//   render: function () {
+//     return (
+//       <div className="searchInput input-group col-top col-centered col-md-6">
+//         <label for="searchInput">Search</label>
+//         <input id="searchInput" className="form-control" type="text" ref="searchInput" placeholder="Name, Description, etc..." onChange={this.searchInputHandler}></input>
+//       </div>
+//     );
+//   }
+// });
 
-var StatusInput = React.createClass({displayName: "StatusInput",
-  statusInputHandler: function () {
-    var statuses = [];
-    for (var key in this.refs) {
-      if (this.refs[key].getDOMNode().checked) {
-        statuses.push(this.refs[key].getDOMNode().value);
-      }
-    }
-    this.props.statusInputHandler(statuses);
-  },
-  render: function () {
-    return (
-      React.createElement("div", {className: "statusInput input-group col-top col-centered col-md-2"}, 
-        React.createElement("label", {className: "checkbox"}, "Status"), 
-        React.createElement("div", {className: "checkbox"}, 
-          React.createElement("label", null, 
-            React.createElement("input", {type: "checkbox", value: "Operating", ref: "operatingInput", onChange: this.statusInputHandler}), 
-            "Operating"
-          )
-        ), 
-        React.createElement("div", {className: "checkbox"}, 
-          React.createElement("label", null, 
-            React.createElement("input", {type: "checkbox", value: "Exited", ref: "exitedInput", onChange: this.statusInputHandler}), 
-            "Exited"
-          )
-        ), 
-        React.createElement("div", {className: "checkbox"}, 
-          React.createElement("label", null, 
-            React.createElement("input", {type: "checkbox", value: "Dead", ref: "deadInput", onChange: this.statusInputHandler}), 
-            "Dead"
-          )
-        )
-      )
-    );
-  }
-});
+// var StatusInput = React.createClass({
+//   statusInputHandler: function () {
+//     var statuses = [];
+//     for (var key in this.refs) {
+//       if (this.refs[key].getDOMNode().checked) {
+//         statuses.push(this.refs[key].getDOMNode().value);
+//       }
+//     }
+//     this.props.statusInputHandler(statuses);
+//   },
+//   render: function () {
+//     return (
+//       <div className="statusInput input-group col-top col-centered col-md-2">
+//         <label className="checkbox">Status</label>
+//         <div className="checkbox">
+//           <label>
+//             <input type="checkbox" value="Operating" ref="operatingInput" onChange={this.statusInputHandler}></input>
+//             Operating
+//           </label>
+//         </div>
+//         <div className="checkbox">
+//           <label>
+//             <input type="checkbox" value="Exited" ref="exitedInput" onChange={this.statusInputHandler}></input>
+//             Exited
+//           </label>
+//         </div>
+//         <div className="checkbox">
+//           <label>
+//             <input type="checkbox" value="Dead" ref="deadInput" onChange={this.statusInputHandler}></input>
+//             Dead
+//           </label>
+//         </div>
+//       </div>
+//     );
+//   }
+// });
 
-var ClassInput = React.createClass({displayName: "ClassInput",
-  classInputHandler: function () {
-    var classes = [];
-    for (var key in this.refs) {
-      if (this.refs[key].getDOMNode().checked) {
-        classes.push(this.refs[key].getDOMNode().value);
-      }
-    }
-    this.props.classInputHandler(classes);
-  },
-  render: function () {
-    return (
-      React.createElement("div", {className: "classInput input-group col-top col-centered col-md-2"}, 
-        React.createElement("label", {className: "checkbox"}, "Class"), 
-        React.createElement("div", {className: "checkbox"}, 
-          React.createElement("label", null, 
-            React.createElement("input", {type: "checkbox", value: "6", ref: "summerInput", onChange: this.classInputHandler}), 
-            "Summer"
-          )
-        ), 
-        React.createElement("div", {className: "checkbox"}, 
-          React.createElement("label", null, 
-            React.createElement("input", {type: "checkbox", value: "1", ref: "winterInput", onChange: this.classInputHandler}), 
-            "Winter"
-          )
-        )
-      )
-    );
-  }
-});
+// var ClassInput = React.createClass({
+//   classInputHandler: function () {
+//     var classes = [];
+//     for (var key in this.refs) {
+//       if (this.refs[key].getDOMNode().checked) {
+//         classes.push(this.refs[key].getDOMNode().value);
+//       }
+//     }
+//     this.props.classInputHandler(classes);
+//   },
+//   render: function () {
+//     return (
+//       <div className="classInput input-group col-top col-centered col-md-2">
+//         <label className="checkbox">Class</label>
+//         <div className="checkbox">
+//           <label>
+//             <input type="checkbox" value="6" ref="summerInput" onChange={this.classInputHandler}></input>
+//             Summer
+//           </label>
+//         </div>
+//         <div className="checkbox">
+//           <label>
+//             <input type="checkbox" value="1" ref="winterInput" onChange={this.classInputHandler}></input>
+//             Winter
+//           </label>
+//         </div>
+//       </div>
+//     );
+//   }
+// });
 
-var YearInput = React.createClass({displayName: "YearInput",
-  yearInputHandler: function () {
-    var years = [];
-    for (var key in this.refs) {
-      if (this.refs[key].getDOMNode().checked) {
-        years.push(this.refs[key].getDOMNode().value);
-      }
-    }
-    this.props.yearInputHandler(years);
-  },
-  render: function () {
-    return (
-      React.createElement("div", {className: "yearInput input-group col-top col-centered col-md-3"}, 
-        React.createElement("label", {className: "checkbox"}, "Year"), 
-        React.createElement("div", {className: "row row-top"}, 
-          React.createElement("div", {className: "controls col-md-4"}, 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2015", ref: "2015Input", onChange: this.yearInputHandler}), 
-                "2015"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2014", ref: "2014Input", onChange: this.yearInputHandler}), 
-                "2014"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2013", ref: "2013Input", onChange: this.yearInputHandler}), 
-                "2013"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2012", ref: "2012Input", onChange: this.yearInputHandler}), 
-                "2012"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2011", ref: "2011Input", onChange: this.yearInputHandler}), 
-                "2011"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2010", ref: "2010Input", onChange: this.yearInputHandler}), 
-                "2010"
-              )
-            )
-          ), 
-          React.createElement("div", {className: "controls col-md-4"}, 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2009", ref: "2009Input", onChange: this.yearInputHandler}), 
-                "2009"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2008", ref: "2008Input", onChange: this.yearInputHandler}), 
-                "2008"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2007", ref: "2007Input", onChange: this.yearInputHandler}), 
-                "2007"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2006", ref: "2006Input", onChange: this.yearInputHandler}), 
-                "2006"
-              )
-            ), 
-            React.createElement("div", {className: "checkbox"}, 
-              React.createElement("label", null, 
-                React.createElement("input", {type: "checkbox", value: "2005", ref: "2005Input", onChange: this.yearInputHandler}), 
-                "2006"
-              )
-            )
-          )
-        )
-      )
-    );
-  }
-});
+// var YearInput = React.createClass({
+//   yearInputHandler: function () {
+//     var years = [];
+//     for (var key in this.refs) {
+//       if (this.refs[key].getDOMNode().checked) {
+//         years.push(this.refs[key].getDOMNode().value);
+//       }
+//     }
+//     this.props.yearInputHandler(years);
+//   },
+//   render: function () {
+//     return (
+//       <div className="yearInput input-group col-top col-centered col-md-3">
+//         <label className="checkbox">Year</label>
+//         <div className="row row-top">
+//           <div className="controls col-md-4">
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2015" ref="2015Input" onChange={this.yearInputHandler}></input>
+//                 2015
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2014" ref="2014Input" onChange={this.yearInputHandler}></input>
+//                 2014
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2013" ref="2013Input" onChange={this.yearInputHandler}></input>
+//                 2013
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2012" ref="2012Input" onChange={this.yearInputHandler}></input>
+//                 2012
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2011" ref="2011Input" onChange={this.yearInputHandler}></input>
+//                 2011
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2010" ref="2010Input" onChange={this.yearInputHandler}></input>
+//                 2010
+//               </label>
+//             </div>
+//           </div>
+//           <div className="controls col-md-4">
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2009" ref="2009Input" onChange={this.yearInputHandler}></input>
+//                 2009
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2008" ref="2008Input" onChange={this.yearInputHandler}></input>
+//                 2008
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2007" ref="2007Input" onChange={this.yearInputHandler}></input>
+//                 2007
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2006" ref="2006Input" onChange={this.yearInputHandler}></input>
+//                 2006
+//               </label>
+//             </div>
+//             <div className="checkbox">
+//               <label>
+//                 <input type="checkbox" value="2005" ref="2005Input" onChange={this.yearInputHandler}></input>
+//                 2005
+//               </label>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// });
 
-var FundingSlider = React.createClass({displayName: "FundingSlider",
-  render: function () {
-    return (
-      React.createElement("div", null, "Funding Slider")
-    );
-  }
-});
+// var FundingSlider = React.createClass({
+//   render: function () {
+//     return (
+//       <div>Funding Slider</div>
+//     );
+//   }
+// });
 
-var ExitSlider = React.createClass({displayName: "ExitSlider",
-  render: function () {
-    return (
-      React.createElement("div", null, "Exit Slider")
-    );
-  }
-});
+// var ExitSlider = React.createClass({
+//   render: function () {
+//     return (
+//       <div>Exit Slider</div>
+//     );
+//   }
+// });
 
-var SearchForm = React.createClass({displayName: "SearchForm",
-  searchInputHandler: function (searchString) {
-    this.props.searchInputHandler(searchString);
-  },
-  statusInputHandler: function (statuses) {
-    this.props.statusInputHandler(statuses);
-  },
-  classInputHandler: function (classes) {
-    this.props.classInputHandler(classes);
-  },
-  yearInputHandler: function (years) {
-    this.props.yearInputHandler(years);
-  },
-  fundingSliderHandler: function () {
-    this.props.fundingSliderHandler();
-  },
-  exitSliderHandler: function () {
-    this.props.exitSliderHandler();
-  },
-  render: function () {
-    return (
-      React.createElement("div", {className: "searchForm"}, 
-        React.createElement("div", {className: "row row-centered"}, 
-          React.createElement(SearchInput, {searchInputHandler: this.searchInputHandler})
-        ), 
-        React.createElement("div", {className: "row row-centered"}, 
-          React.createElement(StatusInput, {statusInputHandler: this.statusInputHandler}), 
-          React.createElement(ClassInput, {classInputHandler: this.classInputHandler}), 
-          React.createElement(YearInput, {yearInputHandler: this.yearInputHandler})
-        ), 
-        React.createElement("div", {className: "row row-centered"}, 
-          React.createElement(FundingSlider, {fundingSliderHandler: this.fundingSliderHandler})
-        ), 
-        React.createElement("div", {className: "row row-centered"}, 
-          React.createElement(ExitSlider, {exitSliderHandler: this.exitSliderHandler})
-        )
-      )
-    );
-  }
-});
+// var SearchForm = React.createClass({
+//   searchInputHandler: function (searchString) {
+//     this.props.searchInputHandler(searchString);
+//   },
+//   statusInputHandler: function (statuses) {
+//     this.props.statusInputHandler(statuses);
+//   },
+//   classInputHandler: function (classes) {
+//     this.props.classInputHandler(classes);
+//   },
+//   yearInputHandler: function (years) {
+//     this.props.yearInputHandler(years);
+//   },
+//   fundingSliderHandler: function () {
+//     this.props.fundingSliderHandler();
+//   },
+//   exitSliderHandler: function () {
+//     this.props.exitSliderHandler();
+//   },
+//   render: function () {
+//     return (
+//       <div className="searchForm">
+//         <div className="row row-centered">
+//           <SearchInput searchInputHandler={this.searchInputHandler}></SearchInput>
+//         </div>
+//         <div className="row row-centered">
+//           <StatusInput statusInputHandler={this.statusInputHandler}></StatusInput>
+//           <ClassInput classInputHandler={this.classInputHandler}></ClassInput>
+//           <YearInput yearInputHandler={this.yearInputHandler}></YearInput>
+//         </div>
+//         <div className="row row-centered">
+//           <FundingSlider fundingSliderHandler={this.fundingSliderHandler}></FundingSlider>
+//         </div>
+//         <div className="row row-centered">
+//           <ExitSlider exitSliderHandler={this.exitSliderHandler}></ExitSlider>
+//         </div>
+//       </div>
+//     );
+//   }
+// });
 
 /* ---------- Company List View ---------- */
 
-// To use company logos, use the following as the src image
-// src={"https://logo.clearbit.com/" + this.props.data.imageUrl} onerror="this.src = '../assets/yc-logo.png'"
 var CompanyCell = React.createClass({displayName: "CompanyCell",
   clickHandler: function () {
     console.log('Cell clicked!');
@@ -268,6 +266,8 @@ var CompanyCell = React.createClass({displayName: "CompanyCell",
       exitedAtValue = '$' + accounting.formatNumber(exitedAtValue);
     }
 
+    // To use company logos, use the following as the src image
+    // src={"https://logo.clearbit.com/" + this.props.company.imageUrl} onerror="this.src = '../assets/yc-logo.png'"
     return (
       React.createElement("div", {className: "companyCell col-centered col-md-9", onClick: this.clickHandler}, 
         React.createElement("div", {className: "row"}, 
@@ -412,7 +412,7 @@ var App = React.createClass({displayName: "App",
     return (
       React.createElement("div", {className: "Main container"}, 
         React.createElement("div", {className: "row row-centered"}, 
-          React.createElement("h1", {className: "col-top col-centered"}, "Y Combinator List")
+          React.createElement("h1", {className: "col-top col-centered"}, "Y Combinator Companies")
         ), 
         React.createElement(SearchForm, {
           searchInputHandler: this.searchInputHandler, 
